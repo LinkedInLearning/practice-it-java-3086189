12e0b9947f3dd9abb299a70b734cc3223e45418d
LimYanZhenGrace <138642098+LimYanZhenGrace@users.noreply.github.com>
2023-07-05 06:38:14 +0000

    Primitive variables done

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -5,36 +5,47 @@ public class Employee {
   public static void main(String[] args) {
 
     // Create a variable called age of type int and assign it the value 29.
+    int age = 29;
 
     // Print the age variable to the console.
+    System.out.println(age);
 
     // Create a variable called isAManager of type boolean and assign it the value
     // true.
+    boolean isAManager = true;
 
     // Print the isAManager variable to the console.
+    System.out.println(isAManager);
 
     // Create a variable called yearsOfService of type double and assign it the
     // value 2.5.
+    double yearsOfService = 2.5;
 
     // Print the yearsOfService variable to the console.
+    System.out.println(yearsOfService);
 
     // Create a variable called baseSalary of type int and assign it the value 3000.
+    int baseSalary = 3000;
 
     // Create a variable called overtimePayment of type int and assign it the value
     // 40.
+    int overtimePayment = 40;
 
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
+    int totalPayment = baseSalary + overtimePayment;
 
     // Print the totalPayment variable to the console.
+    System.out.println(totalPayment);
 
     // Create three variables all of type double on a single line.
     // They should be called firstBonus, secondBonus and thirdBonus and they should
     // be assigned the values 10.00, 22.00 and 35.00.
+    double firstBonus = 10.00, secondBonus = 22.00, thirdBonus = 35.00;
 
     // Print out the sum of the variables called firstBonus, secondBonus and
     // thirdBonus.
-
+    System.out.println(firstBonus + secondBonus + thirdBonus);
   }
 
 }
