309c316eda176799925d2b01552f42003b91b169
Muzzo <51519604+Muzzzo@users.noreply.github.com>
2024-03-16 14:19:47 +0000

    Ticket main method complete

--- _02_05b/Ticket.java
+++ _02_05b/Ticket.java
@@ -12,8 +12,25 @@ public class Ticket {
 
   // Add three public methods to set the value of each field, called
   // setDestination, setPrice and setIsReturn.
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+  public void setPrice(double price){
+    this.price = price;
+  }
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
 
   // Add three public methods to get the value of each field, called
   // getDestination, getPrice and getIsReturn.
-
+  public String getDestination(){
+    return destination;
+  }
+  public double getPrice(){
+    return price;
+  }
+  public boolean getIsReturn(){
+    return isReturn;
+  }
 }
