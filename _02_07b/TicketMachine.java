ec1ece93756eaf9156e55a68dd83b446328bb6a7
Muzzo <51519604+Muzzzo@users.noreply.github.com>
2024-03-16 16:03:47 +0000

    GradingSystem

--- _02_07b/TicketMachine.java
+++ _02_07b/TicketMachine.java
@@ -1,23 +1,27 @@
 package _02_07b;
 
 public class TicketMachine {
-  
-public static void main(String[] args) {
-  // Create an object called ticket of type Ticket
 
-  // Set the destination of the ticket to New York
+  public static void main(String[] args) {
+    // Create an object called ticket of type Ticket
+    Ticket ticket = new Ticket();
 
-  // Set the price of the ticket to 15.30
+    // Set the destination of the ticket to New York
+    ticket.setDestination("New York");
 
-  // Set the isReturn value to true
+    // Set the price of the ticket to 15.30
+    ticket.setPrice(15.30);
 
-  // Print the ticket's destination to the console
+    // Set the isReturn value to true
+    ticket.setIsReturn(true);
+    // Print the ticket's destination to the console
+    System.out.println(ticket.getDestination());
 
-  // Print the ticket's price to the console
+    // Print the ticket's price to the console
+    System.out.println(ticket.getPrice());
 
-  // Print the ticket's isReturn value to the console
-
-}
-
+    // Print the ticket's isReturn value to the console
+    System.out.println(ticket.getIsReturn());
+  }
 
 }
--- _03_03b/GradingSystem.java
+++ _03_03b/GradingSystem.java
@@ -5,7 +5,12 @@ public class GradingSystem {
   public boolean isAPass(int percentage) {
     // Return true if the percentage is higher than or equal to 60.
     // Otherwise return false.
-    return false;
+    if (percentage >= 60) {
+      return true;
+    } else {
+      return false;
+
+    }
   }
 
   public char getGrade(int percentage) {
@@ -14,17 +19,41 @@ public class GradingSystem {
     // If it's 70-79, return 'C'.
     // If it's 60-69, return 'D'.
     // If it's less than 60, return 'F'.
-    return 'X';
+    if (percentage >= 90) {
+      return 'A';
+    } else if (percentage >= 80 && percentage <= 89) {
+      return 'B';
+    } else if (percentage >= 70 && percentage <= 79) {
+      return 'C';
+    } else if (percentage <= 60 && percentage <= 69) {
+      return 'D';
+    } else if (percentage < 60) {
+      return 'F';
+    } else {
+      return 'X';
+    }
   }
 
   public String retakeMessage(int percentage, boolean allowedToRetake) {
     // If percentage is less than 60 and allowedToRetake is true, return a String
+    if (percentage < 60 && allowedToRetake == true) {
+      return "he student has been entered for a retake.";
+    }
     // that says "The student has been entered for a retake."
     // If percentage is less than 60 and allowedToRetake is false, return a String
+    if (percentage < 60 && allowedToRetake == false) {
+      return "he student is not allowed to retake this exam.";
+    }
     // that says "The student is not allowed to retake this exam."
     // If percentage is 60 or higher, return a String that says "A retake is not
+    if (percentage >= 60) {
+      return "A retake is not required";
+    }
     // required."
-    return "";
+    else {
+      return "";
+    }
+
   }
 
 }
